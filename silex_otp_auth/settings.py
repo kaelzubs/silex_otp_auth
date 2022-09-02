ce2b4231675542cc4385f278b06ed35862bf5787
kaelzubs <donmart4u@gmail.com>
2022-09-02 13:02:51 +0100

    django

--- silex_otp_auth/settings.py
+++ silex_otp_auth/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://silex-otp-auth.herokuapp.com/', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
