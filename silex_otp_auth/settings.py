1e3a4ae3cd380c1afc0e4c6d5cd5fdeb8f8a3288
kaelzubs <donmart4u@gmail.com>
2022-09-02 12:32:08 +0100

    gunicorn

--- silex_otp_auth/settings.py
+++ silex_otp_auth/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://silex-otp-auth.herokuapp.com/']
+ALLOWED_HOSTS = ['https://silex-otp-auth.herokuapp.com/', '*']
 
 
 # Application definition
