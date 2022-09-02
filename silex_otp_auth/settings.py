552212d08f8efd18c2d6f42a2370453c3cb58e4e
kaelzubs <donmart4u@gmail.com>
2022-09-02 13:41:54 +0100

    update

--- silex_otp_auth/settings.py
+++ silex_otp_auth/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-
-django_heroku.settings(locals())
+import django_heroku, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +39,6 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,7 +49,8 @@ INSTALLED_APPS = [
     
     'codes',
     'users',
-    'crispy_forms'
+    'crispy_forms',
+    'whitenoise.runserver_nostatic',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -89,6 +87,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'silex_otp_auth.wsgi.application'
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 
 # Database
@@ -135,11 +141,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
--- silex_otp_auth/wsgi.py
+++ silex_otp_auth/wsgi.py
@@ -7,10 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silex_otp_auth.settings')
+
+# application = get_wsgi_application()
+
+
+import os
+import sys
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silex_otp_auth.settings')
+
+path = '/home/kaelzubs/OTP_DJANGO/'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'silex_otp_auth.settings'
+
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root='/home/kaelzubs/OTP_DJANGO/silex_otp_auth/static')
+application.add_files('/home/kaelzubs/OTP_DJANGO/silex_otp_auth/static/img/', prefix='static_img')
